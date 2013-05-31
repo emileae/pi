af3c018efe8b0b1d0d62d1b75c1eb168a4fd156e
Emile Esterhuizen <emile.app@gmail.com>
2013-05-31 19:25:34 +0200

    some styling and animations

--- pi_check.js
+++ pi_check.js
@@ -8,28 +8,19 @@ $(document).ready(function(){
     /*$('#frame').css('width', $(window).width());
     $('#frame').css('height', $(window).height());*/
     
+    //setting the #result_footer height to prevent crazy bouncing for first few digits
+    $('#result_footer').css('height', '2em');
+    
     //Select Mode
-    var challenge = true;
-    $('#record_attempt').css('background-color', 'rgba(0,0,0,0.9)');
-    $('#record_attempt').css('color', 'rgba(255,255,255,0.9)');
-    var practice = false;
+    var challenge = false;
+    $('#practice').css('background-color', 'rgba(0,0,0,0.9)');
+    $('#practice').css('color', 'rgba(255,255,255,0.9)');
+    var practice = true;
     
     var pi = '1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989';
     
     // Clear Result
     $('#key_clear').on('touchend', clear_result);
-    
-    // Study button
-    $('#study').on('touchend', function(){
-        $('body').append('<div id="overlay"><div id="overlay_close">Close</div></div><div id="study_pi"><div class="scrollable">3.<br/>'+pi+'</div></div>');
-        study_scroll = new iScroll('study_pi', {hScrollbar: false, vScrollbar: true, lockDirection: true });
-        
-    });
-    
-    $('body').on('touchend', '#overlay #overlay_close', function(){
-        $('#overlay').remove();
-        $('#study_pi').remove();
-    });
     
     function clear_result (){
         $('#results_log').text('3.');
@@ -56,6 +47,7 @@ $(document).ready(function(){
             $('#record_attempt').css('color', 'rgba(255,255,255,0.9)');
             $('#practice').css('color', 'black');
             $('#practice').css('background-color', 'rgba(0,0,0,0.2)');
+            $('#study').html('<del>Study</del>')
         }else if (practice){
             $('#practice').css('background-color', 'rgba(0,0,0,0.9)');
             $('#practice').css('color', 'rgba(255,255,255,0.9)');
@@ -64,6 +56,21 @@ $(document).ready(function(){
             clear_result();
         };
     };
+    
+    // Study button
+    $('#study').on('touchend', function(){
+        if (practice){
+            
+            $('body').append('<div id="overlay"><div id="overlay_close">Close</div></div><div id="study_pi"><div class="scrollable">&#960 = 3.<br/>'+pi+'</div></div>');
+            study_scroll = new iScroll('study_pi', {hScrollbar: false, vScrollbar: true, lockDirection: true });
+        };
+        
+    });
+    
+    $('body').on('touchend', '#overlay #overlay_close', function(){
+        $('#overlay').remove();
+        $('#study_pi').remove();
+    });
     
     
     // Key color changes
@@ -86,7 +93,10 @@ $(document).ready(function(){
         //checking input digits
         if($(this).text() == pi[current_pos-1]){
             
-            $('#results_log').append($(this).text());
+            //check for landmarks
+            if(current_pos == 10){var lm_class = '10'};
+            $('#current_position').addClass('lm_background_'+lm_class+'')
+            $('#results_log').append('<span class="lm_'+lm_class+'">'+$(this).text()+'</span>');
             result_scroll.scrollToElement('#result_footer', 100);
             
             result_scroll.refresh();
