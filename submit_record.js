06db51d45ffecb53b215114847a3f020c76348d5
Emile Esterhuizen <emile.app@gmail.com>
2013-06-02 22:35:19 +0200

    updated for submission

--- submit_record.js
+++ submit_record.js
@@ -4,36 +4,66 @@ $(document).ready(function(){
         
             $('body').on('touchend', '#record', function(){
                 
-                $('body').append('\
-                    <div id="record_submit_overlay">\
-                        <div id="overlay_close">Close</div>\
-                    </div>\
-                    \
-                    <div id="record_submit_form_container">\
-                        <div>Your Score is : '+localStorage.record_pos+'</div>\
+                if (localStorage.name_id){
+                    $('body').append('\
+                        <div id="record_submit_overlay">\
+                            <div id="overlay_close">Close</div>\
+                        </div>\
                         \
-                        <br>\
-                        \
-                        <form action="emile-pi.appspot.com/submit_record" method="post" id="pi_score_form">\
-                            \
-                            <label for="name">\
-                                <b>Please enter your name</b>\
-                                <input type="text" id="pi_name" name="name">\
-                            </label>\
-                                <input type="hidden" id="pi_score" name="score" value="'+localStorage.record_pos+'">\
+                        <div id="record_submit_form_container">\
+                            <div>Your Score is : '+localStorage.record_pos+' your id is: '+localStorage.name_id+' your rank is: '+localStorage.rank+'</div>\
                             \
                             <br>\
                             \
-                            <input class="submit_btn" type="submit" value="submit">\
+                            <form action="emile-pi.appspot.com/submit_record" method="post" id="pi_score_form">\
+                                \
+                                    <input type="hidden" id="pi_name_id" name="name_id" value="'+localStorage.name_id+'">\
+                                    <input type="hidden" id="pi_name" name="name" value="">\
+                                    <input type="hidden" id="pi_score" name="score" value="'+localStorage.record_pos+'">\
+                                \
+                                <br>\
+                                \
+                                <input class="submit_btn" type="submit" value="submit">\
+                                \
+                            </form>\
+                            <div id="form_loading_indicator" style="width:100%; text-align:center;">Loading</div>\
+                        </div>\
+                        \
+                    ')
+                    
+                    $('#form_loading_indicator').hide();
+                }else{
+                    $('body').append('\
+                        <div id="record_submit_overlay">\
+                            <div id="overlay_close">Close</div>\
+                        </div>\
+                        \
+                        <div id="record_submit_form_container">\
+                            <div>Your Score is : '+localStorage.record_pos+'</div>\
                             \
-                        </form>\
-                        <div id="form_loading_indicator" style="width:100%; text-align:center;">Loading</div>\
-                    </div>\
-                    \
-                ')
-                
-                $('#form_loading_indicator').hide();
-                
+                            <br>\
+                            \
+                            <form action="emile-pi.appspot.com/submit_record" method="post" id="pi_score_form">\
+                                \
+                                <label for="name">\
+                                    <b>Please enter your name</b>\
+                                    <input type="text" id="pi_name" name="name">\
+                                </label>\
+                                    <input type="hidden" id="pi_name_id" name="name_id" value="">\
+                                    <input type="hidden" id="pi_score" name="score" value="'+localStorage.record_pos+'">\
+                                \
+                                <br>\
+                                \
+                                <input class="submit_btn" type="submit" value="submit">\
+                                \
+                            </form>\
+                            <div id="form_loading_indicator" style="width:100%; text-align:center;">Loading</div>\
+                        </div>\
+                        \
+                    ')
+                    
+                    $('#form_loading_indicator').hide();
+                };
             });
         
     $('body').on('touchend', '#overlay_close', close_submit_overlay);
@@ -48,6 +78,7 @@ $(document).ready(function(){
         $('#form_loading_indicator').show();
         
         var name = $('#pi_name').val();
+        var name_id = $('#pi_name_id').val();
         var score = $('#pi_score').val();
          
         $.ajax({
@@ -58,7 +89,10 @@ $(document).ready(function(){
                 console.log(data);
                 $('#form_loading_indicator').hide();
                 alert('Your record was successfully submitted');
-                $('#checker').text(data);
+                $('#checker').text(data[rank]);
+                localStorage.rank = data[rank];
+                localStorage.name_id = data[id];
+                localStorage.name = data[name]
                 close_submit_overlay();
             },
             error: function(data){
